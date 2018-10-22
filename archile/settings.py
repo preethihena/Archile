b79eab8ea6ef3c9a41955f4faedf75d8fb3a77a7
Dhruv <dhruv.faadu@gmail.com>
2018-10-22 22:35:32 +0530

    Frontend for login page.

--- archile/settings.py
+++ archile/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -86,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'archile',
         'USER': 'root',
-        'PASSWORD': 'krishna',
+        'PASSWORD': 'Dhruv!23',
         'HOST': 'localhost',
         'PORT': '',
     }
@@ -128,3 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
