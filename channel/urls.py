9e0108b2f1f32195252539f5be75106af6525a73
Dhruv <dhruvroshan.m16@iiits.in>
2018-10-23 12:15:27 +0530

    Search backend navigations

--- channel/urls.py
+++ channel/urls.py
@@ -2,5 +2,8 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('',index,name="index")
+	#use same name for url, view function and "name" : Ex:path('asd',asd,name="asd"),
+    path('',index,name="index"),
+    path('search/<str:query>',search,name="search"),
+    path('search_box',search_box,name="search_box"),
 ]
--- channel/views.py
+++ channel/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
 	return render(request,'archile/index.html')
+
+def search(request,query):
+	
+	if query:
+		print(query)
+	#if improper query....
+	return render(request, 'archile/search.html')
+
+def search_box(request):
+
+	if request.method == 'POST':
+		for i in request.POST:
+			print(i,"\t",request.POST[i])
+		return redirect(search,request.POST)
+	else:
+		pass
+	return render(request, 'archile/search_box.html')
