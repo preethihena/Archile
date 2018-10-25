b791e293709da5745a346efd8c6fa50bd53b19b7
vishakhakhurangale <vishakhashashikant.k16@iiits.in>
2018-10-26 04:36:22 +0530

    channel page

--- channel/views.py
+++ channel/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-	return render(request,'archile/base.html')
+	return render(request,'archile/channel.html')
 
 
 def login(request):
