b255407ff2eb9c5c58f299c2f091dbce2b87a28b
Dhruv <dhruvroshan.m16@iiits.in>
2018-10-27 00:39:07 +0530

    updated base form

--- archile/settings.py
+++ archile/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'archile.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'archile',
+        'ENGINE': 'django.db.backends.sqlite3',
+        
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'root',
         'PASSWORD': 'Dhruv!23',
         'HOST': 'localhost',
